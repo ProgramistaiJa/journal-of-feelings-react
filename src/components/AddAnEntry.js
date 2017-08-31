9bf552b9e1f4b7321cb9251ab1143b58d0329455
ProgramistaiJa <programista@i-ja.pl>
2017-08-31 15:35:46 +0200

    Added  allFeelings to state in AddAnEntry

--- src/components/AddAnEntry.js
+++ src/components/AddAnEntry.js
@@ -24,7 +24,153 @@ class AddAnEntry extends React.Component {
         date: this.props.date || moment().format('YYYY-MM-D'),
         feelings: this.props.feelings || '',
         when: this.props.when || '',
-        thoughts: this.props.thoughts || ''
+        thoughts: this.props.thoughts || '',
+        allfeelings:{
+                    miłość: [
+                            'miłość',
+                            'ciepło',
+                            'serdeczność',
+                            'podziw',
+                            'czułość',
+                            'pociąg',
+                            'przyjaźń',
+                            'fascynacja',
+                            'troskliwość'
+                            ],
+
+                    pragnienie: [
+                            'pragnienie',
+                            'tęsknota',
+                            'nienasycenie',
+                            'pożadanie',
+                            'potrzeba',
+                            'ciekawość',
+                            'dociekliwość'
+                            ],
+
+
+                    nadzieja: [
+                            'nadzieja',
+                            'wyczekiwanie',
+                            'wiara',
+                            'ufność',
+                            'pewność siebie',
+                            'pogoda ducha',
+                            'akceptacja',
+                            'współczucie'
+                            ],
+
+                    radość: [
+                            'radość',
+                            'szczęście',
+                            'zadowolenie',
+                            'przyjemność',
+                            'satysfakcja',
+                            'spełnienie',
+                            'wesołość',
+                            'zachwyt',
+                            'spokój wewnętrzny',
+                            'łagodność',
+                            'odprężenie',
+                            'ulga',
+                            'wdzięczność'
+                            ],
+
+                    nienawiść : [
+                            'nienawiść',
+                            'obojętność',
+                            'odraza',
+                            'niechęć',
+                            'obcość',
+                            'pogarda',
+                            'lekceważenie',
+                            'zawiść',
+                            'zazdrość'
+                            ],
+
+                    awersja : [
+                            'awersja',
+                            'odwrócenie się',
+                            'wyobcowanie się',
+                            'odrzucenie'
+                            ],
+
+                    rozpacz : [
+                            'rozpacz',
+                            'zwątpienie',
+                            'sceptycyzm',
+                            'podejrzliwość',
+                            'znięchęcenie',
+                            'niezadowolenie',
+                            'przygnębienie',
+                            'bezradność',
+                            'zagubienie',
+                            'poczucie klęski',
+                            'bezsilność'
+                            ],
+
+                    smutek : [
+                            'smutek',
+                            'żal',
+                            'niezadowolenie',
+                            'przykrość',
+                            'rozżalenie',
+                            'strapienie',
+                            'uraza',
+                            'poczucie krzywdy',
+                            'przygnębienie',
+                            'wstyd',
+                            'zakłopotanie',
+                            'upokorzenie',
+                            'przejęcie'
+                            ],
+
+                    odwaga: [
+                            'odwaga',
+                            'śmiałość',
+                            'dzielność',
+                            'cierpliwość',
+                            'wytrwałość',
+                            'pewność siebie',
+                            'pewność',
+                            'zuchwałość',
+                            'zadziorność'
+                            ],
+
+                    strach : [
+                            'strach',
+                            'przerażenie',
+                            'panika',
+                            'lęk',
+                            'przestrach',
+                            'zastraszenie',
+                            'obawa',
+                            'niepokój',
+                            'nerwowość',
+                            'niecierpliwość',
+                            'zdenerwowanie',
+                            'zatrwożenie',
+                            'zmartwienie',
+                            'niepewność',
+                            'nieufność',
+                            'zdumienie',
+                            'zaskoczenie'
+                            ],
+
+                    złość : [
+                            'złość',
+                            'furia',
+                            'wściekłość',
+                            'gniew',
+                            'oburzenie',
+                            'irytacja',
+                            'strapienie',
+                            'uraza',
+                            'wrogość',
+                            'niechęć',
+                            'rozdrażnienie'
+                            ]
+                    }
     }
 
     handleSubmit = event => {
